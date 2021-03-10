77b4978175ad4e5baf1b0f018b188cf35c31698e
Rafiat <danijurafiat@gmail.com>
2021-03-10 23:01:48 +0100

    removes all previous folders

--- styles/library-styles/BookCardListStyles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const BookCardListStyles = styled.div`
-    display:grid;
-    grid-template-columns:repeat(4,1fr);
-    gap:30px;
-`
-
-export default BookCardListStyles
